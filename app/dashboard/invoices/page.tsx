3fe4f1a9c984f076dbea937fafb66bab0aa7a13b
omancillav <edgarmncllav04@gmail.com>
2025-08-01 19:23:33 -0600

    Refactor Page component to handle searchParams as a Promise for improved async handling

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,9 +7,9 @@ import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import { fetchInvoicesPages } from "@/app/lib/data";
 
-export default async function Page({ searchParams }: { searchParams: { query?: string; page?: string } }) {
-  const query = searchParams.query || "";
-  const currentPage = Number(searchParams.page) || 1;
+export default async function Page({ searchParams }: { searchParams: Promise<{ query?: string; page?: string }> }) {
+  const { query = "", page } = await searchParams;
+  const currentPage = Number(page) || 1;
   const totalPages = await fetchInvoicesPages(query);
 
   return (
