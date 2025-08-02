09dca7a0f03cf683686af48184584a1623771e3e
omancillav <edgarmncllav04@gmail.com>
2025-08-01 19:20:42 -0600

    Update page function to accept 'page' as a string in searchParams

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -7,7 +7,7 @@ import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 import { fetchInvoicesPages } from "@/app/lib/data";
 
-export default async function Page({ searchParams }: { searchParams: { query?: string; page?: number } }) {
+export default async function Page({ searchParams }: { searchParams: { query?: string; page?: string } }) {
   const query = searchParams.query || "";
   const currentPage = Number(searchParams.page) || 1;
   const totalPages = await fetchInvoicesPages(query);
